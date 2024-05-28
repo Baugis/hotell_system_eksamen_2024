27e880930668a45f68ba5005bd94972148e05bbe
HanneEriksen <bergumhanne@hotmail.com>
2024-05-28 13:24:59 +0200

    innsjekkinger

--- src/main/java/org/eksamen/Entity/Innsjekkinger.java
+++ src/main/java/org/eksamen/Entity/Innsjekkinger.java
@@ -1,4 +1,24 @@
 package org.eksamen.Entity;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 public class Innsjekkinger {
+    private int innsjekkingsid;
+
+    private int reservasjonsid;
+
+    private LocalDateTime innsjekkingdato;
+
+    public int getInnsjekkingsid() {
+        return innsjekkingsid;
+    }
+
+    public int getReservasjonsid() {
+        return reservasjonsid;
+    }
+
+    public void setInnsjekkingsid(int innsjekkingsid) {this.innsjekkingsid = innsjekkingsid;}
+
+    public void setReservasjonsid(int reservasjonsid) {this.reservasjonsid = reservasjonsid;}
 }
