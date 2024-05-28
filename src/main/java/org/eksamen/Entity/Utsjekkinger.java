713aea321a3f78d68d4198ed8c82111026c9994e
bergisen <morten.bergmann4@gmail.com>
2024-05-28 14:15:27 +0200

    reservasjoner v2

--- src/main/java/org/eksamen/Entity/Utsjekkinger.java
+++ src/main/java/org/eksamen/Entity/Utsjekkinger.java
@@ -1,4 +1,43 @@
 package org.eksamen.Entity;
 
+import java.time.LocalDateTime;
+
 public class Utsjekkinger {
+
+
+    private int utsjekkingid;
+
+    private int reservasjonid;
+
+    private LocalDateTime utsjekkingdato;
+
+    public Utsjekkinger(int utsjekkingid, int reservasjonid, LocalDateTime utsjekkingdato){
+        this.utsjekkingid = utsjekkingid;
+        this.reservasjonid = reservasjonid;
+        this.utsjekkingdato = utsjekkingdato;
+    }
+
+    private int getUtsjekkingid(){
+        return utsjekkingid;
+    }
+
+    private int getReservasjonid(){
+        return reservasjonid;
+    }
+
+    private LocalDateTime getUtsjekkingdato(){
+        return utsjekkingdato;
+    }
+
+    public void setUtsjekkingid(int utsjekkingid){
+        this.utsjekkingid = utsjekkingid;
+    }
+
+    public void setReservasjonid(int reservasjonid){
+        this.reservasjonid = reservasjonid;
+    }
+
+    public void setUtsjekkingdato(LocalDateTime utsjekkingdato){
+        this.utsjekkingdato = utsjekkingdato;
+    }
 }
