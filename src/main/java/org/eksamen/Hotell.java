67386f6613e19be10251358a996d50a2b12c4f9c
HanneEriksen <bergumhanne@hotmail.com>
2024-05-28 15:39:24 +0200

    lagt inn tekst

--- src/main/java/org/eksamen/Hotell.java
+++ src/main/java/org/eksamen/Hotell.java
@@ -17,10 +17,48 @@ public class Hotell {
     }
 
     // FUNKSJONALITETER UNDER
+    // KUNDE
+
+        // Opprette bruker?
+        // Det skal være mulig å lage bruker
+        // Påvirker kunde
 
         // Søkealgoritme
-        // Søke på rom etter tilgjengelige rom, pris og romtype
+        // Bruker skal søke på rom etter tilgjengelige rom, pris og romtype
         // Rommet kan ikke være reservert i tidsperioden bruker søker etter rommet
+        // Bruker skal kunne oppgi en fra-til pris
+        // Bruker skal kunne oppgi ønsket romtype
+
+        // Reservere rom
+        // Bruker skal kunne bestille/reservere rom
+        // Bruker må skrive inn kundeid, startdato og sluttdato
+        // Dette påvirker reservasjonstabellen / reservasjonslisten
+
+        // Avbestille rom
+        // Bruker skal kunne avbestille egen reservasjon
+        // Bruker må skrive inn kundeid for å få opp reservasjonen og bekrefte avbestilling
+        // Reservasjonstabellen / reservasjonslisten
+
+    // RESEPSJON
+        // Innsjekking av kunde
+        // Resepsjonen skal kunne sjekke inn en kunde ved hjelp av reservasjonsid
+        // Opprettes innsjekkingsid og innsjekkingsdato
+        // Påvirker innsjekkingstabellen/liste
+
+        // Utsjekking av kunde
+        // Resepsjonen skal kunne sjekke ut en kunde ved hjelp av reservasjonsid
+        // Får en utsjekkingsdato time
+
+    // ADMINISTRASJON
+        // Legge til rom
+        // Det skal være mulig å legge til et nytt rom
+        // Romid, romnummer, romtype, pris
+        // Påvirker romtabell / liste
+
+        // Slette rom
+        // Det skal være mulig å slette et rom
+
+
 
 
 }
