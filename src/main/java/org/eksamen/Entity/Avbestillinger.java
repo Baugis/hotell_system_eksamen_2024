b8ca183fa7b9d91cf372850253f32337f48033cb
HanneEriksen <bergumhanne@hotmail.com>
2024-05-28 13:09:33 +0200

    test

--- src/main/java/org/eksamen/Entity/Avbestillinger.java
+++ src/main/java/org/eksamen/Entity/Avbestillinger.java
@@ -1,4 +1,21 @@
 package org.eksamen.Entity;
 
 public class Avbestillinger {
+    private String avbestillingsid;
+
+    private String reservasjonsid;
+
+    private String avbestillingdato;
+
+    public String getAvbestillingsid() {
+        return avbestillingsid;
+    }
+
+    public String getReservasjonsid() {
+        return reservasjonsid;
+    }
+
+    public String getAvbestillingdato() {
+        return avbestillingdato;
+    }
 }
