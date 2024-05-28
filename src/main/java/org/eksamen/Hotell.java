338217cdaaa2670bdfeb54f9cd84aa5a7cdfac11
kulmixip <anders_b96@hotmail.com>
2024-05-28 13:44:21 +0200

    Lister opprettet

--- src/main/java/org/eksamen/Hotell.java
+++ src/main/java/org/eksamen/Hotell.java
@@ -1,4 +1,14 @@
 package org.eksamen;
 
 public class Hotell {
+
+    private Liste liste;
+
+    // Opprette Liste objekt
+    // Hente data fra database
+    public Hotell() {
+        liste = new Liste();
+    }
+
+    // Funksjonalitet
 }
